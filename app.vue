1accecd8e948c679c68f56edd18d40f1805ee680
RayShaw <784452262@qq.com>
2018-06-01 14:56:02 +0800

    commit

---
+++ app.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div>Hello {{name}}</div>
+    <div>
+      <v-title title="Vue组件化"></v-title>
+      <v-button @click="handleClick">点击按钮</v-button>
+    </div>
+    <p><img src="./images/image.png" alt="test" style="width:200px"></p>
+  </div>
+</template>
+<script>
+import vTitle from "./title.vue";
+import vButton from "./button.vue";
+export default {
+  components: {
+    vTitle,
+    vButton
+  },
+  data() {
+    return {
+      name: "Vue.js"
+    };
+  },
+  methods: {
+    handleClick(e) {
+      console.log(e);
+    }
+  }
+};
+</script>
+<style scoped>
+div {
+  color: #f60;
+  font-size: 24px;
+}
+</style>
---
+++ main.js
@@ -0,0 +1,44 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Routers from "./router.js";
+import Vuex from "vuex";
+import App from "./app.vue";
+import "./style.css";
+
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
+
+// 路由配置
+const RouterConfig = {
+  // 使用HTML5的History路由模式
+  mode: "history",
+  roters: Routers
+};
+
+const router = new VueRouter(RouterConfig);
+
+router.beforeEach((to, from, next) => {
+  window.document.title = to.meta.title;
+  next();
+});
+
+router.afterEach((to, from, next) => {
+  window.scrollTo(0, 0);
+});
+
+const store = new Vuex.Store({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {}
+});
+
+new Vue({
+  el: "#app",
+  router: router,
+  store: store,
+  redner: h => {
+    return h(App);
+  }
+});
